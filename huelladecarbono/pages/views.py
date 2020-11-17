42523455ff7432f4b4100a576333b26632c302e1
Omar Gutiérrez <omar.gutierrezm@utem.cl>
2020-11-17 17:12:24 -0300

    Cambios

--- huelladecarbono/pages/views.py
+++ huelladecarbono/pages/views.py
@@ -2,9 +2,21 @@ from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Page
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
+from .models import Page
+from .forms import PageForm 
+from django.shortcuts import redirect
 
+class StaffRequiredMixin(object):
+    """ 
+    Verifica que el usuario sea staff
+    """
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return redirect(reverse_lazy('admin:login'))
+        return super(StaffRequiredMixin, self).dispatch(request, *args, **kwargs)
+    
 
 class PageListView(ListView):
     model = Page
@@ -12,10 +24,29 @@ class PageListView(ListView):
 class PageDetailView(DetailView):
     model = Page
 
-class PageCreateView(CreateView):
+class PageCreateView(StaffRequiredMixin, CreateView):
     model = Page
-    fields = ['title','content','order']
+    form_class = PageForm
     success_url = reverse_lazy('pages:pages')
+    ### alternativa para comprobar si el usuario esta identificado y pueda usar el crud
+   
+    
+
+
+class PageUpdateView(StaffRequiredMixin, UpdateView):
+    model = Page
+    form_class = PageForm
+    template_name_suffix = '_update_form'
+
+    def get_success_url(self):
+        ### para pasar el identificador y retornar a la pagina en edición
+        return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
+
+
+class PageDeleteView(StaffRequiredMixin, DeleteView):
+    model = Page
+    success_url = reverse_lazy('pages:pages')
+
 
 
 
