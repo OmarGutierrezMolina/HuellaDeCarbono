95a62462ab323e33ac57c51a1b46c8dcbc0e2dd9
Omar Gutiérrez <omar.gutierrezm@utem.cl>
2020-11-19 14:19:23 -0300

    Cambios app no util

--- huelladecarbono/huelladecarbono/urls.py
+++ huelladecarbono/huelladecarbono/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from pages.urls import pages_patterns
 from django.conf import settings
+from profiles.urls import profiles_patterns
 
 urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
     path('pages/', include(pages_patterns)),
+    path('profiles/', include(profiles_patterns)),
     #path auth
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('registration.urls')),
--- huelladecarbono/registration/models.py
+++ huelladecarbono/registration/models.py
@@ -1,8 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 # Create your models here.
+
+#para que se borre lo que habia antes
+def custom_upload_to(instance, filename):
+    old_instance = Profile.objects.get(pk=instance.pk)
+    old_instance.avatar.delete()
+    return 'profiles/' + filename
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to='profiles', null=True, blank=True, height_field=None, width_field=None, max_length=None)
+    avatar = models.ImageField(upload_to=custom_upload_to, null=True, blank=True, height_field=None, width_field=None, max_length=None)
     bio = models.TextField(null=True, blank=True)
     link = models.URLField(null=True, blank=True, max_length=200)
+
+@receiver(post_save, sender=User)
+def ensure_profile_exists(sender, instance, **kwargs):
+    #para que se ejecute solo una vez al crear perfil
+    if kwargs.get('created',False):
+        Profile.objects.get_or_create(user=instance)
+        #crea usuario y perfil enlazado
