4da220e33d104502bc84911f7971e8fbc96b75d3
Omar Gutiérrez <omar.gutierrezm@utem.cl>
2020-11-24 17:33:44 -0300

    prueba en remoto

--- huelladecarbono/huelladecarbono/settings.py
+++ huelladecarbono/huelladecarbono/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'huelladecarbono.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -99,6 +99,13 @@ DATABASES = {
         'PASSWORD':'xu0D7gF9vR2otsQESqSD4SlFAoiJjLan',
         'HOST':'tuffi.db.elephantsql.com',
         'DATABASE_PORT':'5432',
+    }
+}
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 """
