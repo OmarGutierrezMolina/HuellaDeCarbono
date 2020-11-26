138b3daa240fc675a85e71740d375ca541368952
Omar Gutiérrez <omar.gutierrezm@utem.cl>
2020-11-26 15:09:30 -0300

    Dirección editada correctamente

--- huelladecarbono/registration/urls.py
+++ huelladecarbono/registration/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('signup/', SignUpCreateView.as_view(), name='signup'),
     path('profile/', ProfileUpdateView.as_view(), name='profile'),
     path('profile/email/', EmailUpdateView.as_view(), name='profile_email'),
-    path('profile/address/', AddressUpdateView.as_view(), name='profile_address'),
+    path('profile/address/<int:pk>', AddressUpdateView.as_view(), name='profile_address'),
 ]
